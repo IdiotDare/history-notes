49f8f8da9254bb1fd641c41607befd2589cd0cf0
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-16 08:17:17 -0500

    Update cnscript.js

--- ChineseDynasties/cnscript.js
+++ ChineseDynasties/cnscript.js
@@ -2,15 +2,15 @@
 var testText = ["testing","two"];
 document.getElementById("han-text").innerHTML = testText[0];
 
-var infoObj = {
-  shang: [
+var shangInfo = [
           "They extended the kingdom to the Xia Kingdom in the north, and south of the Yellow river.",
           "They created:The Chinese writing system -over 10,000 characters -7 main dialect groups, but hundreds of sub-dialects(because of all the ethnic groups); a common writing system was necessary for unity",
           "They created: The notion of filial piety -respected for parents and the ancestors of the father -generations lived together, the eldest male controlled everything(money,important decisions)",
           "Girls/women were treated as inferior, raised to obey the males in the family. They were married off between the ages of 13-16, becoming part of their husbands' families.",
           "The Shang dynasty was polytheistic with a supreme God, and many less Gods."
-  ],
-  zhou: [
+  ];
+
+var zhouInfo: [
        "The Dynastic Cycle, can be considered the life cycle of a dynasty: 1. Strong dynasty establishes peace and prosperity; considered to have Mandate of Heaven.",
          "The Dynastic Cycle, can be considered the life cycle of a dynasty: 2. Over time, the dynasty becomes corrupt; taxes get raised; power grows weaker.",
          "The Dynastic Cycle, can be considered the life cycle of a dynasty: 3. Disasters(diseases, famine, floods, revolts) happen.",
@@ -26,8 +26,9 @@ var infoObj = {
     "The Mandate of Heaven is a part of the Dynastic Cycle. It is the belief that God favors a family/person to rule China with a \"Mandate from Heaven\". When a Dynasty falls, it is said to have lost God's Mandate from Heaven, and the new dynasty family has been granted it.",
     "The Zhou Empire lasted 900 years, and were attacked by nomadic people from the north, leaving their capital burned and Emperor murdered.",
     "The members of the Royal family scattered throughout the Empire, as local nobles claimed to be Kings of their own States. Seven individual States were established that were constantly at war, known as the Seven Warring States."
-         ],
-  han: [
+         ];
+
+  var hanInfo = [
     "A general, Liu Bang declared himself the first emperor of the Han dynasty after the civil wars and instability following the fall of the Qin dynasty.",
     "He established a centralized government with hundreds of provincials reporting to his government.",
     "To win support, Liu Bang lowered taxes and was less strict with punishments.",
@@ -35,12 +36,11 @@ var infoObj = {
     "The Han government featured a complex bureaucracy, including up to 18 ranks of civil service jobs. Under the Han, bureaucrats were selected to civil service jobs based on their knowledge of Confucianism.",
     "The Han dynasty promoted cultural assimilation among the people they conquered as a way to unify Chinese culture.",
     "Eventually, the disparity between the rich and the poor led to this dynasty's downfall. The rulers immediately following the Han proved to be inexperienced and unsuccessful rulers."
-     ]
-}
+     ];
+
 //random numbers
 function randNum(prop){
- let num = Math.floor(Math.random()*(infoObj.prop.length-1))
- return infoObj.prop[num]
+ let num = Math.floor(Math.random()*(varia.length-1))
 }
 //button functions
 function shangChange(){
@@ -60,5 +60,5 @@ function zhouChange(){
 }
 
 function hanChange(){
-  document.getElementById('han-text').innerHTML = infoObj.han[3];
+  document.getElementById('han-text').innerHTML = hanInfo[3];
 }
