58f46f03701ced04331a778ec1ef3d46537f572b
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-16 08:24:25 -0500

    Update cnscript.js

--- ChineseDynasties/cnscript.js
+++ ChineseDynasties/cnscript.js
@@ -48,7 +48,7 @@ function shangChange(){
 document.getElementById('shang-text').innerHTML = testText[1];
 
 }
-
+/*
 function zhouChange(){
   let num = Math.floor(Math.random()*(infoObj.prop.length - 1));
   let text = infoObj.zhou[num];
@@ -62,3 +62,4 @@ function zhouChange(){
 function hanChange(){
   document.getElementById('han-text').innerHTML = hanInfo[3];
 }
+*/
