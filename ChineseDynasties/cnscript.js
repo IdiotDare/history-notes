85cb2aacea030fafbd3305ae29ca9187adcc9aa8
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-16 07:23:22 -0500

    Update cnscript.js

--- ChineseDynasties/cnscript.js
+++ ChineseDynasties/cnscript.js
@@ -1,56 +1,35 @@
 
-var testText = 'testing';
-document.getElementById('han-text').innerHtml = testText;
+var testText = "testing";
+document.getElementById("han-text").innerHTML = testText;
 
 var infoObj = {
-  shang: ["They extended the kingdom to the Xia Kingdom in the north, and south of the Yellow river.",
+  shang: [
+          "They extended the kingdom to the Xia Kingdom in the north, and south of the Yellow river.",
           "They created:The Chinese writing system -over 10,000 characters -7 main dialect groups, but hundreds of sub-dialects(because of all the ethnic groups); a common writing system was necessary for unity",
           "They created: The notion of filial piety -respected for parents and the ancestors of the father -generations lived together, the eldest male controlled everything(money,important decisions)",
           "Girls/women were treated as inferior, raised to obey the males in the family. They were married off between the ages of 13-16, becoming part of their husbands' families.",
-          "The Shang dynasty was polytheistic with a supreme God, and many less Gods."],
-  zhou: ["The Dynastic Cycle, can be considered the life cycle of a dynasty: 1. Strong dynasty establishes peace and prosperity; considered to have Mandate of Heaven.",
-         "The Dynastic Cycle, can be considered the life cycle of a dynasty.:
-
-
-
+          "The Shang dynasty was polytheistic with a supreme God, and many less Gods."
+  ],
+  zhou: [
+       "The Dynastic Cycle, can be considered the life cycle of a dynasty: 1. Strong dynasty establishes peace and prosperity; considered to have Mandate of Heaven.",
+         "The Dynastic Cycle, can be considered the life cycle of a dynasty: 2. Over time, the dynasty becomes corrupt; taxes get raised; power grows weaker.",
+         "The Dynastic Cycle, can be considered the life cycle of a dynasty: 3. Disasters(diseases, famine, floods, revolts) happen.",
+         "The Dynastic Cycle, can be considered the life cycle of a dynasty: 4. Because said dynasty is seen as having lost the Mandate of Heaven(God's approval), it justifies rebellions.",
+         "The Dynastic Cycle, can be considered the life cycle of a dynasty: 5. Dynasty gets overthrown through revolts and bloodshed, and a new dynasty emerges.",
+         "The Dynastic Cycle, can be considered the life cycle of a dynasty: 6. The new dynasty gains power; restores peace and order, claims to have Mandate of Heaven.",
+    "The Zhou dynasty started the concept of the Dynastic Cycle and the Mandate of Heaven as an excuse to overthrow the Shang.",
+    "Accomplishments: Introduction of the feudal system to China as they divded their lands into regions to be governed by a family member",
+    "Accomplishments: construction of roads and canals -stimulates trade and farming",
+    "Accomplishments: Standard system of coin -makes trade easier, no money conversion needed and varying values",
+    "Blast furnaces -produces iron weapons and tools",
+    "The Dynastic Cycle is the process where a dynasty rises, rules for a while, declines, gets replaced, and the process repeats.",
+    "The Mandate of Heaven is a part of the Dynastic Cycle. It is the belief that God favors a family/person to rule China with a \"Mandate from Heaven\". When a Dynasty falls, it is said to have lost God's Mandate from Heaven, and the new dynasty family has been granted it.",
+    "The Zhou Empire lasted 900 years, and were attacked by nomadic people from the north, leaving their capital burned and Emperor murdered.",
+    "The members of the Royal family scattered throughout the Empire, as local nobles claimed to be Kings of their own States. Seven individual States were established that were constantly at war, known as the Seven Warring States."
+         ],
+  han: [
+    
 }
-
-
-var wLines = [
-    "I know right?",
-    "Confusing, huh?",
-    "I don't know what you want.",
-    "I'm not sure why you're asking me.",
-    "I don't think you should ask me.",
-    "I...",
-    "I...am not exactly sure.",
-    "I don't get it...",
-    "Sorry. I don't understand.",
-    "I don't know. Do I look like I even care? ...Sorry.",
-    "I don't know what you want.",
-    "I'm going to f*cking slap you.",
-    "...?",
-    "I am not...",
-    "*Local ??? is confused!*",
-    "no idea dude I’m lost",
-    "Not sure bro I’m unsure",
-    "I...forget how to think. Later?",
-    "Umm..."
-] //an array of possible options which can be picked out later with smth ike wLines[num] 
-
-function change_text(){
-    var num = Math.floor(Math.random() * (17-1)) //chooses a random number between 0 and the number of text options, which will then be used to index blah blah the array.
-    if(wLines[num] != document.getElementById("text").textContent) {
-        document.getElementById("text").innerHTML = wLines[num];
-    } else if(wLines[num] == document.getElementById("text").textContent) {
-        rantext()
-    } else {
-        document.getElementById("text").innerHTML = "IdiotDare is an idiot and needs to fix their code.";
-    }
-}
-
-
-
 
 //button functions
 function shangChange(){
